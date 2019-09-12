21c886d4eae6d75f6d2ce48dddf2c1d916a80735
ariabee <abrown72@uvm.edu>
2019-09-11 23:44:38 -0400

    navbar updates with javascript

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+/*!
+  * Portfolio javascript functions
+  * AriaRay Brown, 9-2019
+  */
+
+function toggleNavBar() {  
+	
+	let mainNav = document.getElementById('js-menu');
+	let navBarToggle = document.getElementById('js-navbar-toggle');
+
+	navBarToggle.addEventListener('click', function () {
+	    
+	    mainNav.classList.toggle('active');
+	});
+}
+
