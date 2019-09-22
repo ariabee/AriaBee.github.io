9813233e0edbb2910b5524b0616a4a40ce42f38a
ariabee <abrown72@uvm.edu>
2019-09-22 13:17:13 -0400

    added jQuery

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,7 @@
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
-navBarToggle.addEventListener('click', function () {
+navBarToggle.addEventListener('click', function() {
     
     mainNav.classList.toggle('active');
 });
